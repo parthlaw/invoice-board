18387d865d206aae4ee8fb8d3b380db925d3a0eb
Sanchita Gupta <56127516+sanchita1900@users.noreply.github.com>
2020-12-22 20:38:54 +0530

    login page

--- registration/urls.py
+++ registration/urls.py
@@ -6,6 +6,6 @@ app_name = "registration"
 
 urlpatterns = [
     
-    path('register/', views.register_request, name='register'),
+    path('', views.register_request, name='register'),
     path('login/', views.login_request, name='login'),
 ]
