6ac18906c2662fcc6e7ce3c23234533c67c00810
sejaljain123 <sejaljain2043@gmail.com>
2020-12-27 21:08:01 +0530

    Shop Form

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # Create your views here.
-
+from .forms import ShopForm
 
 def main(request):
     if request.user.is_authenticated:
@@ -8,3 +8,20 @@ def main(request):
         return render(request, 'nav/nav.html', {"User": User})
     else:
         return redirect('/login')
+
+def index(request):
+    form=ShopForm()
+   
+    if request.user.is_authenticated:
+        User = request.user
+        if request.method =='POST':
+            form=ShopForm(request.POST)
+            if form.is_valid():
+                form.save()
+            
+
+        return render(request,'main/index.html',context={'form':form})
+    else:
+        return redirect('/login')
+
+        
