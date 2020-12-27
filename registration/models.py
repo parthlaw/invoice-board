6825b55a7cebe847da3969dc900edf6df7564b4c
sejaljain123 <sejaljain2043@gmail.com>
2020-12-27 18:10:35 +0530

    models

--- registration/models.py
+++ registration/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=42)
+    created_on = models.DateTimeField(auto_now_add=True)
+    address=models.TextField()
+    
