85683a4b67297c1e395fdeb988d0bc4c3d713c9b
parthlaw <parth@LAPTOP-KLMP3S0O.localdomain>
2020-12-28 16:40:25 +0530

    added gitignore

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ app_name = "main"
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('home/', views.test, name='test')
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render, redirect
 def main(request):
     if request.user.is_authenticated:
         User = request.user
+        print("HI tets")
         return render(request, 'nav/nav.html', {"User": User})
     else:
         return redirect('/login')
+
+
+def test(request):
+    return render(request, 'main/test.html')
