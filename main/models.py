f6dbf7c47decaaed6fd4897703cdef5b5868233d
Sanchita Gupta <56127516+sanchita1900@users.noreply.github.com>
2021-01-06 11:03:24 +0530

    Merge branch 'sejal' of https://github.com/parthlaw/invoice-board into sanchita

--- main/models.py
+++ main/models.py
@@ -1,53 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Shop(models.Model):
     shop_name = models.CharField(max_length=42)
     created_on = models.DateTimeField(auto_now_add=True)
-    address=models.TextField()
+    address = models.TextField()
 
-    class Meta:
-        db_table="Shop"
 
 class item(models.Model):
+    TABLET='TB'
+    CAPSULE='CP'
+    SYRUP='SY'
+    LOTION='LO'
+    DROP='DR'
+    OINTMENT='OI'
+    POWDER='PW'
+    SOAP='SP'
+    OTHERS='OT'
+    ITEM_TYPE_CHOICES=[(TABLET,'Tablet'),(CAPSULE,'Capsule'),(SYRUP,'Syrup'),(LOTION,'Lotion'),
+        (DROP,'Drop'),(OINTMENT,'Ointment'),(POWDER,'Powder'),(SOAP,'Soap'),(OTHERS,'Others'),]
     item_name = models.CharField(max_length=42)
-    cost_price= models.FloatField()
-    company=models.TextField()
-    item_type=models.TextField()
- 
-    class Meta:
-        db_table="item"
+    company = models.TextField()
+    item_type = models.TextField(choices=ITEM_TYPE_CHOICES)
+  
 
 class stock(models.Model):
-    shop_id = models.ForeignKey(Shop,on_delete=models.CASCADE)
-    item_id =  models.ForeignKey(item,on_delete=models.CASCADE)
-    Date= models.DateTimeField(auto_now_add=True)
-    quantity=models.IntegerField()
+    shop_id = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(item, on_delete=models.CASCADE)
+    Date = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField()
     MRP = models.FloatField()
-    mfg_date= models.DateTimeField(auto_now_add=True)
-    expiry= models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        db_table="stock"
+    cost_price = models.FloatField()
+    mfg_date = models.DateTimeField(auto_now_add=True)
+    expiry = models.DateTimeField(auto_now_add=True)
+
 
 class sales(models.Model):
     invoice_no = models.IntegerField()
-    item_id =  models.ForeignKey(item,on_delete=models.CASCADE)
-    shop_id =  models.ForeignKey(Shop,on_delete=models.CASCADE)
-    quantity=  models.IntegerField()
-    cus_name=   models.CharField(max_length=42)
-    sell_price= models.FloatField()
-
-    class Meta:
-        db_table="sales"
+    stock_id = models.ForeignKey(stock, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(item, on_delete=models.CASCADE)
+    shop_id = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    cus_name = models.CharField(max_length=42)
+    sell_price = models.FloatField()
 
 
 class inventory(models.Model):
-    item_id =  models.ForeignKey(item,on_delete=models.CASCADE)
-    shop_id =  models.ForeignKey(Shop,on_delete=models.CASCADE)
-    quantity= models.IntegerField()
-
-    class Meta:
-        db_table="inventory"
-
-    
+    item_id = models.ForeignKey(item, on_delete=models.CASCADE)
+    shop_id = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
